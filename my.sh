a18da6e2e2742bc00dc9b5a95ea79b4b60a67fe8
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-04-09 05:10:57 +0700

    Add files via upload

---
+++ my.sh
@@ -0,0 +1,107 @@
+#jangan ganti author script nya tod"
+sleep 2
+clear
+sleep 1
+echo -e "\e[91mhacked.by M3R4Z\e[0m";
+sleep 1.8
+clear
+sleep 1
+echo -e "\e[92mNCI=NEW CYBER INDONESIA\e[0m";
+sleep 1.5
+clear
+sleep 2
+echo -e "\e[94m•tool heck diamon free fire\e[0m";
+sleep 1
+echo -e "\e[91m_________________________________________\e[0m";
+sleep 1
+echo -e "\e[92m• Author.        = Ardy\e[0m";
+sleep 1
+echo -e "\e[92m• no WA.         = +6283192644730\e[0m";
+sleep 0.5
+echo -e "\e[92m• chennel yt     = MR M3R4Z\e[0m";
+sleep 1
+echo -e "\e[92m• Tim.           = NCI=NEW CYBER INDONESIA\e[0m";
+sleep 0.6
+echo -e "\e[91m_______________________________________\e[0m";
+sleep 1
+echo -e "\e[94m• tool ini tidak 100% work\e[0m";
+sleep 1
+echo -e "\e[95m• masukan ID akun free fire Anda\e[0m";
+sleep 0.3
+read -p '$>' userinput1;
+#
+sleep 2
+echo -e "\e[37m1•memperoses..................
+\e[0m";
+sleep 1
+echo -e "\e[96m2•memperoses.....................\e[0m";
+clear
+sleep 0.6
+echo -e "\e[95m3•memperoses........................\e[0m";
+sleep 1
+clear
+echo -e "\e[94m4•memperoses...........................\e[0m";
+sleep 1.8
+clear
+sleep 1
+echo -e "\e[93m5•memperoses..............................\e[0m";
+sleep 1
+clear
+echo -e "\e[92m6•memperoses.................................\e[0m";
+sleep 1
+echo -e "\e[91m7•memperoses....................................\e[0m";
+sleep 1
+clear
+sleep 2
+echo -e "\e[91m-------------------------------------------------------------\e[0m";
+sleep 0.6
+echo -e "\e[92m•diamon free fire\e[0m";
+sleep 0.8
+echo -e "\e[91m-------------------------------------------------------------\e[0m";
+sleep 1
+echo -e "\e[93m•masukan jumlah diamon\e[0m";
+sleep 0.9
+read -p '$>' userinput1;
+#
+sleep 1
+echo -e "\e[91msedang memperoses..................\e[0m";
+sleep 1
+clear
+sleep 1
+echo -e "\e[92msedang memperoses.....................\e[0m";
+sleep 1
+clear
+sleep 0.8
+echo -e "\e[94msedang memperoses........................\e[0m";
+sleep 1.5
+clear
+sleep 1
+echo -e "\e[94msedang memperoses...........................\e[0m";
+sleep 1
+clear
+sleep 1
+echo -e "\e[93msedang memperoses..............................\e[0m";
+sleep 2
+clear
+sleep 2
+echo -e "\e[93m•loading...\e[0m";
+sleep 1
+echo -e "\e[92m•loading......\e[0m";
+sleep 1
+echo -e "\e[94m
+•loading.........\e[0m";
+sleep 1
+echo -e "\e[91m•loading............\e[0m";
+sleep 1
+echo -e "\e[94m•loading...............\e[0m";
+sleep 2
+echo -e "\e[92m-------------------------------------------------------------------\e[0m";
+sleep 2
+echo -e "\e[94m•tunggu 10 menit diamon anda akan terkirim\e[0m";
+sleep 1
+echo -e "\e[91m•jika diamon anda tidak masuk berarti tool ini sudah tidak work !\e[0m";
+sleep 2
+echo -e "\e[37mketik exit untuk keluar \e[0m";
+read -p '$>' userinput2;
+#
+sleep 3
